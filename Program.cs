efd3749957dbeba9a13e9438a49fd4fd494e8a9f
lucyL <lucylaurentino@gmail.com>
2018-04-09 11:43:52 +1200

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace arrayClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var rnd = new Random();
+            var arr = new int[10];
+            for (var i = 0; i < 10; i++)//give you the index
+            {
+                var r1 = rnd.Next(1, 10);
+                arr[i] = r1;
+            }
+
+            
+            foreach (var x in arr)//give you the value
+            {
+                var r1 = rnd.Next(1, 10);
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine();
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+
+
+        }
+
+
+    }
+}
+
